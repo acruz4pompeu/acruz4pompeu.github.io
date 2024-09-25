49c8da3e4a2cc56011617b3e45070ce8c91d3f7c
acruz4pompeu <acruz4@iespompeufabra.cat>
2024-09-25 10:35:24 +0200

    Update sketch.js

--- cara/sketch.js
+++ cara/sketch.js
@@ -1,28 +1,35 @@
 function setup() 
-{
-  createCanvas(600,400);
-  mic=new p5.AudioIn();
-  mic.start();
 }
 
 function draw() {
+  let pseuRan= noise(frameCount*0.1);
+  let ranX = map(pseuRan,0,1,0,600);
+  let ranX = map(pseuRan,0,1,0,400);
+  let cX =300;
+  let cY =250;
   let vol= mic.getLevel();
   let vulls= map(vol,0,1,0,100);
   let vboca= map(vol,0,1,0,300);
+  console.log("vulls is "+vulls)
+  console.log("vol is "+vol)
   background(127);
   //Cara
-  fill(100,100,100);
-  ellipse(300,200,250,300);
+  fill(227,192,139);
+  ellipse(cX,cY,250,300);
   //Ull dret
   fill(255);
-  ellipse(250,150,50,50);
+  ellipse(cX-50,cY-50,50,50);
   fill(84,42,14);
-  ellipse(250,150,15,15);
+  ellipse(cX-50,cY-50,10*sin(frameCount*0.1)+15,15);
   //Ull esquerre
   fill(255);
-  ellipse(350,150,50,50);
+  ellipse(cX+50,cY-50,50,50);
   fill(84,42,14);
-  ellipse(350,150,15,15);
+  ellipse(cX+50,cY-50,10*cos(frameCount*0.1)+15,15);
   //Boca
   fill(200,15,15);
-  arc(300,250,100,10+vboca,0,PI,CHORD);
+  arc(cX,cY+50,100,10+vboca,0,PI,CHORD);
+  //Nas
+  fill(219,169,116)
+  triangle(300,200,300,225,275-vulls,212.5);
+}
