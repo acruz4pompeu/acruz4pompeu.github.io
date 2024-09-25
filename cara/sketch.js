3f8507bbbf197252484506795bd74c1ae70f7fd5
acruz4pompeu <acruz4@iespompeufabra.cat>
2024-09-25 11:09:10 +0200

    Update sketch.js

--- cara/sketch.js
+++ cara/sketch.js
@@ -16,22 +16,24 @@ function draw() {
   console.log("vol is "+vol)
   background(127);
   //Cara
-  fill(227,192,139);
+  fill(0,0,0);
   ellipse(cX,cY,250,300);
   //Ull dret
   fill(255);
   ellipse(cX-50,cY-50,50,50);
-  fill(84,42,14);
+  fill(255,0,0);
   ellipse(cX-50,cY-50,10*sin(frameCount*0.1)+15,15);
   //Ull esquerre
   fill(255);
   ellipse(cX+50,cY-50,50,50);
-  fill(84,42,14);
+  fill(255,0,0);
   ellipse(cX+50,cY-50,10*cos(frameCount*0.1)+15,15);
   //Boca
   fill(200,15,15);
   arc(cX,cY+50,100,10+vboca,0,PI,CHORD);
-  //Nas
-  fill(219,169,116)
-  triangle(300,200,300,225,275-vulls,212.5);
+  }
+  ellipse(cX+50,cY-50,10*cos(frameCount*0.1)+15,15);
+  //Boca
+  fill(200,15,15);
+  arc(cX,cY+50,100,10+vboca,0,PI,CHORD);
 }
